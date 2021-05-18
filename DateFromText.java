08d7a0827a4bc03bd504320e36ea3f273f5f7084
davi1606 <davimonteiro16@gmail.com>
2021-05-17 21:31:51 -0300

    Mudando para receber Apenas Horas e Minutos

--- DateFromText.java
+++ DateFromText.java
@@ -10,11 +10,11 @@ public class DataFromText {
 	public static void main(String[] args) throws ParseException {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Data de entrada(dd/mm/aaaa):");
-		String DataText = sc.next();	// String lida e armazenada
+		System.out.println("Hora da Entrada(K:mm):");
+		String DataTextin = sc.next();	// String lida e armazenada
 		
-		SimpleDateFormat Data1 = new SimpleDateFormat("dd/MM/yyyy");	// Aqui é possivel Escolher o formato
-		Date Data2 = Data1.parse(DataText);	// "Interpreta" o conteudo da string e transforma em Date
+		SimpleDateFormat Data1 = new SimpleDateFormat("K:mm");	// Aqui é possivel Escolher o formato
+		Date Data2 = Data1.parse(DataTextin);	// "Interpreta" o conteudo da string e transforma em Date
 		
 		System.out.println(Data2);	// Printa a data no formato Date
 	}
