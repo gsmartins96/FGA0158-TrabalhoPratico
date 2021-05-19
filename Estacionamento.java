2a637d6b7ab960669581071f340742eddd19142e
Gabriel Martins <gsmartins96@hotmail.com>
2021-05-19 15:21:09 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -55,13 +55,13 @@ public class Estacionamento{
 	private float calculoEstadia(int minutosPermanecidos, float precoMinuto){
 		float valorPago = precoMinuto * minutosPermanecidos;
 		while(minutosPermanecidos >= 60){
-			valorPago -= minutosPermanecidos/60
-			minutosPermanecidos = minutosPermanecidos/60
+			valorPago -= minutosPermanecidos/60;
+			minutosPermanecidos = minutosPermanecidos/60;
 		}
 
 		while(minutosPermanecidos >= 15){
 			valorPago -= (minutosPermanecidos/15) * (0.5f);
-			minutosPermanecidos = minutosPermanecidos/15
+			minutosPermanecidos = minutosPermanecidos/15;
 		}
 
 		return valorPago;
